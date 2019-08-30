f868ffac94d7f80429889e41187ac871f1af8a42
urvigodha <45790542+urvigodha@users.noreply.github.com>
2019-08-30 17:33:57 +0530

    Update square.js

--- Urvi/square.js
+++ Urvi/square.js
@@ -41,141 +41,156 @@ function check(){
 	for(var k=0;k<25;k++){
 		c+=s[k];
 	}	
-	if(c==15 || c==14){
+	if(c==15 || c==14 || c==13) {
 		var  fail=0;
 		while(1){
 			    //check for 1x1 squses
-    if(s[0] && s[1] && s[5] && s[6])
-        { fail=1; break; }
-    else if(s[1] && s[2] && s[6] && s[7])
-        { fail=1; break; }
-    else if(s[2] && s[3] && s[7] && s[8])
-        { fail=1; break; }
-    else if(s[3] && s[4] && s[8] && s[9])
-        { fail=1; break; }
-    else if(s[5] && s[6] && s[10] && s[11])
-        { fail=1; break; }
-    else if(s[6] && s[7] && s[11] && s[12])
-        { fail=1; break; }
-    else if(s[7] && s[8] && s[12] && s[13])
-        { fail=1; break; }
-    else if(s[8] && s[9] && s[13] && s[14])
-        { fail=1; break; }
-    else if(s[10] && s[11] && s[15] && s[16])
-        { fail=1; break; }
-    else if(s[11] && s[12] && s[16] && s[17])
-        { fail=1; break; }
-    else if(s[12] && s[13] && s[17] && s[18])
-        { fail=1; break; }
-    else if(s[13] && s[14] && s[18] && s[19])
-        { fail=1; break; }
-    else if(s[15] && s[16] && s[20] && s[21])
-        { fail=1; break; }
-    else if(s[16] && s[17] && s[21] && s[22])
-        { fail=1; break; }
-    else if(s[17] && s[18] && s[22] && s[23])
-        { fail=1; break; }
-    else if(s[18] && s[19] && s[23] && s[24])
-        { fail=1; break; }
+            if(s[0] && s[1] && s[5] && s[6])
+                { fail=1; break; }
+            else if(s[1] && s[2] && s[6] && s[7])
+                { fail=1; break; }
+            else if(s[2] && s[3] && s[7] && s[8])
+                { fail=1; break; }
+            else if(s[3] && s[4] && s[8] && s[9])
+                { fail=1; break; }
+            else if(s[5] && s[6] && s[10] && s[11])
+                { fail=1; break; }
+            else if(s[6] && s[7] && s[11] && s[12])
+                { fail=1; break; }
+            else if(s[7] && s[8] && s[12] && s[13])
+                { fail=1; break; }
+            else if(s[8] && s[9] && s[13] && s[14])
+                { fail=1; break; }
+            else if(s[10] && s[11] && s[15] && s[16])
+                { fail=1; break; }
+            else if(s[11] && s[12] && s[16] && s[17])
+                { fail=1; break; }
+            else if(s[12] && s[13] && s[17] && s[18])
+                { fail=1; break; }
+            else if(s[13] && s[14] && s[18] && s[19])
+                { fail=1; break; }
+            else if(s[15] && s[16] && s[20] && s[21])
+                { fail=1; break; }
+            else if(s[16] && s[17] && s[21] && s[22])
+                { fail=1; break; }
+            else if(s[17] && s[18] && s[22] && s[23])
+                { fail=1; break; }
+            else if(s[18] && s[19] && s[23] && s[24])
+                { fail=1; break; }
 
-    //check for 2x2 squses
-    else if(s[0] && s[2] && s[10] && s[12])
-        { fail=1; break; }
-    else if(s[1] && s[3] && s[11] && s[13])
-        { fail=1; break; }
-    else if(s[2] && s[4] && s[12] && s[14])
-        { fail=1; break; }
-    else if(s[5] && s[7] && s[15] && s[17])
-        { fail=1; break; }
-    else if(s[6] && s[8] && s[16] && s[18])
-        { fail=1; break; }
-    else if(s[7] && s[9] && s[17] && s[19])
-        { fail=1; break; }
-    else if(s[10] && s[12] && s[20] && s[22])
-        { fail=1; break; }
-    else if(s[11] && s[13] && s[21] && s[23])
-        { fail=1; break; }
-    else if(s[12] && s[14] && s[22] && s[24])
-        { fail=1; break; }
+            //check for 2x2 squses
+            else if(s[0] && s[2] && s[10] && s[12])
+                { fail=1; break; }
+            else if(s[1] && s[3] && s[11] && s[13])
+                { fail=1; break; }
+            else if(s[2] && s[4] && s[12] && s[14])
+                { fail=1; break; }
+            else if(s[5] && s[7] && s[15] && s[17])
+                { fail=1; break; }
+            else if(s[6] && s[8] && s[16] && s[18])
+                { fail=1; break; }
+            else if(s[7] && s[9] && s[17] && s[19])
+                { fail=1; break; }
+            else if(s[10] && s[12] && s[20] && s[22])
+                { fail=1; break; }
+            else if(s[11] && s[13] && s[21] && s[23])
+                { fail=1; break; }
+            else if(s[12] && s[14] && s[22] && s[24])
+                { fail=1; break; }
 
-    //check for 3x3 squses
-    else if(s[0] && s[3] && s[15] && s[18])
-        { fail=1; break; }
-    else if(s[1] && s[4] && s[16] && s[19])
-        { fail=1; break; }
-    else if(s[5] && s[8] && s[20] && s[23])
-        { fail=1; break; }
-    else if(s[6] && s[9] && s[21] && s[24])
-        { fail=1; break; }
+            //check for 3x3 squses
+            else if(s[0] && s[3] && s[15] && s[18])
+                { fail=1; break; }
+            else if(s[1] && s[4] && s[16] && s[19])
+                { fail=1; break; }
+            else if(s[5] && s[8] && s[20] && s[23])
+                { fail=1; break; }
+            else if(s[6] && s[9] && s[21] && s[24])
+                { fail=1; break; }
 
-    //check for the 4x4 squse
-    else if(s[0] && s[4] && s[20] && s[24])
-        { fail=1; break; }
+            //check for the 4x4 squse
+            else if(s[0] && s[4] && s[20] && s[24])
+                { fail=1; break; }
 
-    //check for 45deg 1x1 squses
-    else if(s[1] && s[5] && s[7] && s[11])
-        { fail=1; break; }
-    else if(s[2] && s[6] && s[8] && s[12])
-        { fail=1; break; }
-    else if(s[3] && s[7] && s[9] && s[13])
-        { fail=1; break; }
-    else if(s[6] && s[10] && s[12] && s[16])
-        { fail=1; break; }
-    else if(s[7] && s[11] && s[13] && s[17])
-        { fail=1; break; }
-    else if(s[8] && s[12] && s[14] && s[18])
-        { fail=1; break; }
-    else if(s[11] && s[15] && s[17] && s[21])
-        { fail=1; break; }
-    else if(s[12] && s[16] && s[18] && s[22])
-        { fail=1; break; }
-    else if(s[13] && s[17] && s[19] && s[23])
-        { fail=1; break; }
+            //check for 45deg 1x1 squses
+            else if(s[1] && s[5] && s[7] && s[11])
+                { fail=1; break; }
+            else if(s[2] && s[6] && s[8] && s[12])
+                { fail=1; break; }
+            else if(s[3] && s[7] && s[9] && s[13])
+                { fail=1; break; }
+            else if(s[6] && s[10] && s[12] && s[16])
+                { fail=1; break; }
+            else if(s[7] && s[11] && s[13] && s[17])
+                { fail=1; break; }
+            else if(s[8] && s[12] && s[14] && s[18])
+                { fail=1; break; }
+            else if(s[11] && s[15] && s[17] && s[21])
+                { fail=1; break; }
+            else if(s[12] && s[16] && s[18] && s[22])
+                { fail=1; break; }
+            else if(s[13] && s[17] && s[19] && s[23])
+                { fail=1; break; }
 
-    //check for the 45deg 2x2 squse
-    else if(s[2] && s[10] && s[14] && s[22])
-        { fail=1; break; }
+            //check for the 45deg 2x2 squse
+            else if(s[2] && s[10] && s[14] && s[22])
+                { fail=1; break; }
 
-    //check for 30ish deg 1x1 squses
-    else if(s[5] && s[2] && s[16] && s[13])
-        { fail=1; break; }
-    else if(s[6] && s[3] && s[17] && s[14])
-        { fail=1; break; }
-    else if(s[10] && s[7] && s[21] && s[18])
-        { fail=1; break; }
-    else if(s[11] && s[8] && s[22] && s[19])
-        { fail=1; break; }
-    else if(s[1] && s[10] && s[17] && s[8])
-        { fail=1; break; }
-    else if(s[2] && s[11] && s[18] && s[9])
-        { fail=1; break; }
-    else if(s[6] && s[15] && s[22] && s[13])
-        { fail=1; break; }
-    else if(s[7] && s[16] && s[23] && s[14])
-        { fail=1; break; }
+            //check for 30ish deg 1x1 squses
+            else if(s[5] && s[2] && s[16] && s[13])
+                { fail=1; break; }
+            else if(s[6] && s[3] && s[17] && s[14])
+                { fail=1; break; }
+            else if(s[10] && s[7] && s[21] && s[18])
+                { fail=1; break; }
+            else if(s[11] && s[8] && s[22] && s[19])
+                { fail=1; break; }
+            else if(s[1] && s[10] && s[17] && s[8])
+                { fail=1; break; }
+            else if(s[2] && s[11] && s[18] && s[9])
+                { fail=1; break; }
+            else if(s[6] && s[15] && s[22] && s[13])
+                { fail=1; break; }
+            else if(s[7] && s[16] && s[23] && s[14])
+                { fail=1; break; }
 
-    //check for slanted 3x3 squses
-    else if(s[5] && s[3] && s[21] && s[19])
-        { fail=1; break; }
-    else if(s[15] && s[1] && s[9] && s[23])
-        { fail=1; break; }
-    else{
-    	//console.log(fail);
-        if(c==15)
-            alert("Correct. 10 dots removed");
-        else if(c==14)
-            alert("Correct- partial - 11 dots removed");
-    	break;
-    }
-		}
-		//console.log(fail);
+            //check for slanted 3x3 squses
+            else if(s[5] && s[3] && s[21] && s[19])
+                { fail=1; break; }
+            else if(s[15] && s[1] && s[9] && s[23])
+                { fail=1; break; }
+            else{
+                console.log("no of dots removed:");
+            	console.log(25-c);
+                if(c==15)
+                    alert("Correct. 10 dots removed- you get 30 points!");
+                else if(c==14)
+                    alert("Correct - 11 dots removed - you get 20 points!");
+                else if(c==13)
+                    alert("Correct - 12 dots removed - you get 10 points!");
+            	else{
+                    c=-1;
+                    console.log("no squares formed but more than 12 removed");
+                }
+
+                break;
+            }
+        }
+		console.log("fail");
+        console.log(fail);
 		if(fail==1){
 			alert("Wrong ans");
+            console.log("squares formed!");
 		}
 		
 
 	}	
 	else{
 		alert("wrong ");
+        c=-1;
+        console.log("squares condition not checked");
+        console.log("no of dots removed:");
+        console.log(25-c);
+        
 	}
 }
